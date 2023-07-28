dbfe5c1b339818d30b0437b65719dcca3b0a69ce
Matheus N <matheusnaschaves@gmail.com>
2023-07-28 16:53:43 -0300

    ws

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -141,6 +141,38 @@ const scriptsInEvents = {
 		item.y<cam.y-runtime.layout.height).forEach(item=>item.destroy());
 	},
 
+	async Egame_Event13_Act1(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.onMessage = (event) => {
+			if (event.data.startsWith('s:')){
+				const score_ = event.data.split(':')[1];
+				console.log(score_);
+				runtime.globalVars.score = score_;
+				runtime.callFunction("updateScore")
+			}
+		}
+	},
+
+	async Egame_Event17_Act1(runtime, localVars)
+	{
+		const queryParams = new URLSearchParams(window.location.search)
+		const token = queryParams.get('token');
+		const gameId = queryParams.get('gameId');
+		
+		try{
+			const webSocket = new WebSocket('wss://arcade.stage.legacyarcade.com/ws', [token,gameId]);
+			runtime.globalVars.webSocket = webSocket;
+			webSocket.onopen = (event) =>{
+				runtime.callFunction('StartTheGame');
+			}; 
+		}catch(e){
+			const textInstance = runtime.objects.ErrorText.getFirstInstance()
+			textInstance.text = "ERROR CONNECTING"
+			console.log("error connecting to server", e)
+		}
+		
+	},
+
 	async Egame_Event25_Act2(runtime, localVars)
 	{
 		//Spawn Jump Particle Effect
@@ -154,6 +186,11 @@ const scriptsInEvents = {
 		const player = runtime.objects.Player.getFirstInstance();
 		runtime.objects.ColorChange.createInstance(1,player.x,player.y).colorRgb = colors[player.instVars.color];
 		runtime.objects.ColorChange.createInstance(1,player.x,player.y).colorRgb = colors[+runtime.globalVars.Temp];
+	},
+
+	async Egame_Event28_Act2(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.send("s:1")
 	},
 
 	async Egame_Event32_Act1(runtime, localVars)
@@ -175,7 +212,7 @@ const scriptsInEvents = {
 
 	async Egame_Event34_Act2(runtime, localVars)
 	{
-		postText(runtime.globalVars.finalScore)
+		runtime.globalVars.webSocket.close();
 	}
 
 };
